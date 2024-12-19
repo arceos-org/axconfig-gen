ae78dd8614ef1dd9a1c344127ed7a50125ac477b
equation314 <equation618@gmail.com>
2024-12-19 17:40:08 +0000

    Deploying to gh-pages from @ arceos-org/axconfig-gen@291195220e41c2310a2d785049c6093279a999a6 🚀

---
+++ crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["axconfig_gen"];
+//{"start":21,"fragment_lengths":[14]}
---
+++ trait.impl/core/convert/trait.From.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["axconfig_gen",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;TomlError&gt; for <a class=\"enum\" href=\"axconfig_gen/enum.ConfigErr.html\" title=\"enum axconfig_gen::ConfigErr\">ConfigErr</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[297]}
